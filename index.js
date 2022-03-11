1aa5ef0469c5a075a41d29ec64751071016b7070
Ndubuisi Ejiogu <ndubuisi.ejiogu@thebulbafrica.institute>
2022-03-11 10:09:54 +0100

    added more changes

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
+// ----------adding categories to the dropdown----------
 const selectCategory = document.querySelector("select");
+const logoDisplay = document.querySelector(".logo-display");
+
 const allCategories = logos.reduce((res, acc) => {
   return res.concat(acc.category);
 }, []);
 const sortedCategories = [...new Set(allCategories)];
-
-// console.log(sortedCategories);
-// console.log(logos);
+console.log(sortedCategories, "jjjjj");
 for (let i = 0; i < sortedCategories.length; i++) {
   const companyCategory = document.createElement("option");
   companyCategory.textContent = sortedCategories[i];
-  companyCategory.setAttribute("value", i);
+  companyCategory.setAttribute("value", sortedCategories[i]);
+  // console.log(companyCategory.setAttribut/e("value", i));
   selectCategory.insertAdjacentElement("beforeend", companyCategory);
 }
-// ----------------------------------------------------------------
-// const catalogue = document.querySelector("div");
-// selectCategory.addEventListener("click", function (params) {
-//   const indexOfCategory = params.target.value;
-//   console.log(logos[indexOfCategory]);
-// });
-
-// function catalogueDisplay(imgLink, tag) {
-//   const displayArea = document.getElementById("displayArea");
-
-// }
-
-const logoDisplay = document.querySelector(".logo-display");
 
 for (let i = 0; i < logos.length; i += 1) {
   const title = logos[i].title;
   const fileName = logos[i].filename;
-  const category = logos[i].category.filter((item) => item.length).join(" / ");
+  const category = logos[i].category.join(" / ");
   const url = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
   logoDisplay.innerHTML += `
   <div class="card-container">
@@ -41,3 +30,33 @@ for (let i = 0; i < logos.length; i += 1) {
   </div>
   `;
 }
+
+// -------------making the categories clickable-----------------
+
+document.getElementById("drop-down").addEventListener("change", function (e) {
+  const indexOfCategory = e.currentTarget.value;
+  logoDisplay.innerHTML = "";
+  for (let i = 0; i < logos.length; i++) {
+    if (logos[i].category.includes(indexOfCategory)) {
+      const title = logos[i].title;
+      console.log(title);
+      const fileName = logos[i].filename;
+      const category = logos[i].category
+        .filter((item) => item.length)
+        .join(" / ");
+      const url = `https://nigerialogos.com/logos/${fileName}/${fileName}.png`;
+      logoDisplay.innerHTML += `
+      <div class="card-container">
+      <div class="image-container">
+      <img src='${url}' class="image">
+      </div>
+      <p class='title'>${title}</p>
+      <h3 class='category'>${category}</h3>
+      </div>
+      `;
+    }
+  }
+});
+
+// ----------------accessing the DOM for the all categories-----------
+console.log(indexOfCategory);
